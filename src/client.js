2808561a41fda6efbdb8b7fe00759f98e40c3f94
Guillermo <Guilleelias25@gmail.com>
2026-01-22 21:30:40 -0600

    n

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Supabase project info
-const supabaseUrl = 'https://qmhnjadnxynbmrzzzcro.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
