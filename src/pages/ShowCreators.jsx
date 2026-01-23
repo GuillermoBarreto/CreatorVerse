58c29253c424eff26f86b9cff2a90167a9c12e66
Guillermo <Guilleelias25@gmail.com>
2026-01-22 21:26:51 -0600

    fixing package.json connection

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { supabase } from '../client.js'
-import CreatorCard from '../components/CreatorCard.jsx'
-import { Link } from 'react-router-dom'
-
-function ShowCreators() {
-  const [creators, setCreators] = useState([])
-
-  useEffect(() => {
-    fetchCreators()
-  }, [])
-
-  async function fetchCreators() {
-    let { data } = await supabase.from('creators').select('*')
+async function fetchCreators() {
+  const { data, error } = await supabase.from('creators').select('*')
+  if (error) {
+    console.log('Supabase error:', error)
+    setCreators([])  // <-- important: prevent null crash
+  } else {
     setCreators(data)
   }
-
-  return (
-    <div>
-      <h1>CreatorVerse</h1>
-      <Link to="/add">
-        <button>Add Creator</button>
-      </Link>
-      {creators.length === 0 && <p>No creators yet!</p>}
-      {creators.map(c => (
-        <CreatorCard key={c.id} creator={c} />
-      ))}
-    </div>
-  )
 }
-
-export default ShowCreators
