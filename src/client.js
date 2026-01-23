2395d5f3d557bd5d348dbb33d665595be98364f4
Guillermo <Guilleelias25@gmail.com>
2026-01-22 21:06:53 -0600

    adding API and supabase URL for the project

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'YOUR_SUPABASE_PROJECT_URL'
-const API_KEY = 'YOUR_SUPABASE_API_KEY'
+const URL = 'https://qmhnjadnxynbmrzzzcro.supabase.co'
+const API_KEY = process.env.SUPABASE_KEY
 
 export const supabase = createClient(URL, API_KEY)
