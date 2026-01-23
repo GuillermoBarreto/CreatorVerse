378059099eac5efce9f946271e14fd1b4f53764c
Guillermo <Guilleelias25@gmail.com>
2026-01-22 21:24:45 -0600

    adding .env to connect with the supabase

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://qmhnjadnxynbmrzzzcro.supabase.co'
-const API_KEY = process.env.SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(URL, API_KEY)
+export const supabase = createClient(supabaseUrl, supabaseKey)
